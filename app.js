25998063830a323d293454e572c3e5cfbff8253d
lestercoyoy <lester_cch@hotmail.com>
2023-02-13 20:20:30 -0600

    ADD
    Upload file with GridFS (It must be changed to GridFS bucket)

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.post('/upload', upload.single('file'), ( req,res) => {
 })
 
 // @routes GET /files
-// @desc dislpay all files in JSON
+// @desc display all files in JSON
 app.get('/files', (req,res) => {
   gfs.files.find().toArray((err, files) => {
     // check if files
@@ -87,7 +87,7 @@ app.get('/files', (req,res) => {
 })
 
 // @routes GET /files/:filename
-// @desc dislpay all files in JSON
+// @desc disply single object in JSON
 app.get('/files/:filename', (req,res) => {
   gfs.files.findOne({filename: req.params.filename}, (err, file) => {
     // check if file
@@ -99,6 +99,30 @@ app.get('/files/:filename', (req,res) => {
 
     //File exists
     return res.json(file);
+  });
+})
+
+// @routes GET /image/:filename
+// @desc display image
+app.get('/image/:filename', (req,res) => {
+  gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+    // check if file
+    if(!file || file.length == 0){
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
   });
 })
 
